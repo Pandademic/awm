eeffd17f2e021d1c68aaedcc4e4f653d52265260
Pandademic <atharv.gupte510@gmail.com>
2022-06-18 13:35:55 -0400

    setup a basic dir structure

--- awm/src/main.rs
+++ awm/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate penrose;
+
+use penrose::{
+    core::{
+        bindings::KeyEventHandler,
+        config::Config,
+        helpers::index_selectors,
+        manager::WindowManager,
+    },
+    logging_error_handler,
+    xcb::new_xcb_backed_winow_manager,
+    Backward, Forward, Less, More, Selector
+};
+
+use simplelog::{LevelFilter, SimpleLogger};
+
+fn main() -> penrose::Result<()> {
+    if let Err(e) = SimpleLogger::init(LevelFilter::Info, simplelog::Config::default()) {
+        panic!("unable to set log level: {}", e);
+    };
+    loop{}
 }
+
